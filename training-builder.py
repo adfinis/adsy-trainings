51128a58362f498358298f22bc387ae59f1bc028
Cyrillvw <cyrill.vonwattenwyl@adfinis-sygroup.ch>
2017-02-20 15:31:07 +0100

    add updated training builder

--- training-builder.py
+++ training-builder.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import glob
 import yaml
 import jinja2
@@ -13,7 +14,8 @@ BUILD_DIR   = "build"
 COMMONS_DIR = "adsy-trainings-common.src"
 TPL_PATH = "adsy-trainings-common.src/index/tpl.html"
 IGNORE_TRAININGS = [
-    "suse-system-administration"
+    "ansible-workshop",
+    "suma-workshop"
 ]
 INDEX_COPY_FILES=[
     "adcssy.min.css",
@@ -37,7 +39,7 @@ def render_template(tpl_path, context):
 
 
 
-def build_markdown(source_file, dest_file, commons_dir):
+def build_html(source_file, dest_file, commons_dir):
     pandoc_cmd_arr = [
         "pandoc",
         "--to",
@@ -54,6 +56,89 @@ def build_markdown(source_file, dest_file, commons_dir):
         dest_file
     ]
     subprocess.check_call(pandoc_cmd_arr)
+
+
+def build_pdf(source_file, dest_file, commons_dir, build_path):
+
+    otmp = "{0}/{1}.tmp.html".format(
+        os.path.dirname( dest_file ),
+        os.path.basename( dest_file )
+    )
+
+    # create a html that works with pdf css
+    pandoc_cmd_arr = [
+        "pandoc",
+        "-c",
+        "../../commons/topdf/style.css",
+        source_file,
+        "-o",
+        otmp
+    ]
+    subprocess.check_call(pandoc_cmd_arr)
+
+    # then convert this to html
+    wkhtmltopdf_cmd_arr = [
+        "wkhtmltopdf",
+        "--page-width",
+        "200",
+        "--page-height",
+        "145",
+        otmp,
+        dest_file
+    ]
+    subprocess.check_call(wkhtmltopdf_cmd_arr)
+
+    # delete temp file
+    os.remove(otmp)
+
+
+
+    #sys.exit(0)
+    # #return
+    # tmp_build_path = "{0}/tmp/tmp-slide-".format(
+    #     build_path
+    # )
+    # # Build single pdf files
+    # phantomjs_cmd_arr = [
+    #     "phantomjs",
+    #     "{0}/phantomjs-revealjs-capture.js".format(
+    #         commons_dir
+    #     ),
+    #     source_file,
+    #     tmp_build_path
+    # ]
+    # print(source_file)
+    # print(" ".join(phantomjs_cmd_arr))
+    #
+    # subprocess.check_call(phantomjs_cmd_arr)
+    #
+    # # combine single pdf's to a single one
+    # tmp_slides = glob.glob(
+    #     "{0}*.pdf".format(
+    #         tmp_build_path
+    #     )
+    # )
+    # tmp_slides = sorted(tmp_slides)
+    # gscmd_array = [
+    #     "gs",
+    #     "-q",
+    #     "-dPDFSETTINGS=/printer",
+    #     "-dNOPAUSE",
+    #     "-dBATCH",
+    #     "-sDEVICE=pdfwrite",
+    #     "-sOutputFile={0}".format(
+    #         dest_file
+    #     )
+    # ]
+    # gscmd_array += tmp_slides
+    # subprocess.check_call(gscmd_array)
+    # #print(" ".join(gscmd_array))
+    #
+    # for tmp_slide in tmp_slides:
+    #     os.remove(tmp_slide)
+
+    #sys.exit(0)
+
 
 
 
@@ -75,6 +160,18 @@ def main():
 
     training_list = {}
 
+    ## Copy commons files first for phantom
+    subprocess.check_call([
+        "cp",
+        "-ra",
+        commons_dir_abs,
+        "{0}/{1}/commons".format(
+            root_dir,
+            BUILD_DIR,
+        )
+    ])
+
+
     for yaml_file in sorted(index_files):
 
         training_dir = os.path.dirname( yaml_file )
@@ -90,6 +187,10 @@ def main():
             root_dir,
             BUILD_DIR,
             training_dir
+        )
+        build_path_root_abs = "{0}/{1}".format(
+            root_dir,
+            BUILD_DIR
         )
 
         yf = open(yaml_file)
@@ -121,13 +222,20 @@ def main():
                 build_path,
                 fname_no_ext
             )
+            pdf_build_file = "{0}/{1}.pdf".format(
+                build_path,
+                fname_no_ext
+            )
+
             tt['files_html'].append(
                 "{0}/{1}.html".format(
                     training_dir,
                     fname_no_ext
                 )
             )
-            build_markdown(md, html_build_file, commons_dir_abs)
+            print("   > {0}.html".format(fname_no_ext))
+            build_html(md, html_build_file, commons_dir_abs)
+
 
         # copy image dirs
         static_dirs = [ name for name in os.listdir(source_dir) if os.path.isdir(os.path.join(source_dir, name)) ]
@@ -150,8 +258,38 @@ def main():
                 dst
             ])
 
+        # then build pdf from generated html
+        for htmlf in tt['files_html']:
+            fname_no_ext = os.path.splitext(
+                os.path.basename( htmlf )
+            )[0]
+            html_build_file = "{0}/{1}.html".format(
+                build_path,
+                fname_no_ext
+            )
+            pdf_output = "{0}/{1}.pdf".format(
+                build_path,
+                fname_no_ext
+            )
+            print("   > {0}.pdf".format(fname_no_ext))
+
+            build_pdf(
+                html_build_file,
+                pdf_output,
+                commons_dir_abs,
+                build_path_root_abs
+            )
+
+            tt['files_pdf'].append(
+                "{0}/{1}.pdf".format(
+                    training_dir,
+                    fname_no_ext
+                )
+            )
+
         # append to dict for index file
         tt['files_html'] = sorted(tt['files_html'])
+        tt['files_pdf']  = sorted(tt['files_pdf'])
         training_list[training_yaml['Area']].append(tt)
 
 
@@ -169,17 +307,8 @@ def main():
         output_index_html
     )
 
-    ## Copy commons file and css
-    subprocess.check_call([
-        "cp",
-        "-ra",
-        commons_dir_abs,
-        "{0}/{1}/commons".format(
-            root_dir,
-            BUILD_DIR,
-        )
-    ])
 
+    # copy css for index
     for css in INDEX_COPY_FILES:
         src = "{0}/index/{1}".format(
             commons_dir_abs,
@@ -195,8 +324,6 @@ def main():
             src,
             dst
         ])
-
-
 
 
 
